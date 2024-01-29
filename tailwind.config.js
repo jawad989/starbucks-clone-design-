69ea694363f5ab6fcee9aee5d1ddd62301a6f4e3
Jawad Awan <101397741+jawad989@users.noreply.github.com>
2024-01-29 21:35:23 +0500

    starbucks landing page clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ module.exports = {
         contentBgBlock: "#A5C4DC",
         gotoBgColor: '#1e3932'
       },
-      fontFamily: {
-        SoDoSans: ['SoDoSans', 'Helvetica Neue']
-      }
     },
   },
   plugins: [],
